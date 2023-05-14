4ea02a642b3f0bf23fd81230f88d50f1197247d1
Mustafa Guney <guneymustafa5937@gmail.com>
2023-05-14 20:49:55 +0300

    V2

--- Lesson01.java
+++ Lesson01.java
@@ -3,6 +3,11 @@ package git01;
 public class Lesson01 {
     public static void main(String[] args) {
         // "java is easy"
+        // version 1
+
+        // version oluşturalım
+        // version 2
+        
 
     }
 }
